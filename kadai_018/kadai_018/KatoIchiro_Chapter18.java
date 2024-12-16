c7c0988275e32d8fba56f69839393007d5cd1615
samurai-inoue <if.99375ne@gmail.com>
2024-12-17 00:08:41 +0900

    課題再提出

---
+++ kadai_018/kadai_018/KatoIchiro_Chapter18.java
@@ -0,0 +1,14 @@
+package kadai_018;
+
+	public class KatoIchiro_Chapter18 extends Kato_Chapter18{
+
+	
+		public void setGivenName() {
+			this.givenName = "一郎";
+		}
+		
+		public void eachIntroduce() {
+			System.out.println("私の好きな食べ物はリンゴです。" + "\n");
+		}
+		
+}
---
+++ kadai_018/kadai_018/Kato_Chapter18.java
@@ -0,0 +1,26 @@
+package kadai_018;
+
+public abstract class Kato_Chapter18{
+ 
+
+	public String familyName = "加藤"; // 姓を表す
+	public String givenName = "";  // 名を表す
+	public String address = "東京都中野区〇×";    // 住所を表す
+
+	
+	//共通の紹介を出力する
+	public void commonIntroduce() {
+		System.out.println("名前は" + familyName + givenName + "です");
+		System.out.println("住所は" + address + "です");
+	}	
+	
+	//個別の紹介を出力する
+	public abstract void eachIntroduce();
+	
+	//紹介を実行する
+	public void execIntroduce() {
+		commonIntroduce();
+		eachIntroduce();
+	}
+	
+}
