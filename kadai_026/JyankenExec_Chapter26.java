9fbf3a1e3afee5c9157c87be2ed681bcf9cef142
samurai-inoue <if.99375ne@gmail.com>
2025-01-21 00:28:55 +0900

    課題の提出

---
+++ kadai_026/JyankenExec_Chapter26.java
@@ -0,0 +1,11 @@
+package kadai_026;
+
+public class JyankenExec_Chapter26 {
+    public static void main(String[] args) {
+        // じゃんけんクラスのインスタンスを作成
+        Janken_Chapter26 jyanken = new Janken_Chapter26();
+
+        // じゃんけんを実行
+        jyanken.playGame();
+    }
+}
